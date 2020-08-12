452d9c07a60139eb745fee45406a5e67667823d5
DESKTOP-KKKJEGF\kds <dd@dd.cd>
2020-08-12 17:33:57 +0900

    Add title write animation

--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -1,19 +1,27 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import PropTypes from 'prop-types';
-import { screenHeight } from './constant';
+import { screenHeight, standardFontSize } from './constant';
+import TypeWriter from 'react-native-typewriter'
 
 const SplashScreen = ({navigation}) => {
 
-    useEffect(()=> {
-        setTimeout(() => {
-            alert('Hi');
-        }, 3000);
-    },[])
+
     
+    const onTypingEnd = () => {
+        setTimeout(() => {
+            navigation.navigate('App')
+        }, 1500);
+    }
 
     return (
         <View style={styles.container}>
+            <TypeWriter 
+                typing={1}
+                maxDelay={200}
+                minDelay={200}
+                onTypingEnd={onTypingEnd}
+                style={textStyles.title}>GoodBye Corona!</TypeWriter>
             <Image source={require('../assets/splashLogo.png')} style={styles.image} />
         </View>
     );
@@ -23,7 +31,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#121b34'
     },
     image: {
         height: screenHeight*0.5,
@@ -32,6 +41,10 @@ const styles = StyleSheet.create({
 });
 
 const textStyles = StyleSheet.create({
+    title: {
+        color: 'white',
+        fontSize: standardFontSize*2
+    }
 });
 
 SplashScreen.propTypes = {
