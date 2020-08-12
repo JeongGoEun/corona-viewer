baf3d9767d90aac506f918f7a1caac17e2434117
DESKTOP-KKKJEGF\kds <wjdrhdms002@naver.com>
2020-08-12 15:46:04 +0900

    Add canvasjs

--- src/Component/Setting.js
+++ src/Component/Setting.js
@@ -4,102 +4,33 @@ import PropTypes from 'prop-types';
 import { Chart } from "react-google-charts";
 import { standardFontSize } from '../constant';
 
+// for CanvasJS
+import CanvasJSReact from '../lib/canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 const Setting = () => {
 
     return (
         <ScrollView>
             <View style={styles.chart}>
-                <Text style={textStyles.title}>한국 확진자수 증가추이</Text>
-                <Chart
-                    width='100%'
-                    height='100%'
-                    chartType="LineChart"
-                    loader={<div>Loading Chart</div>}
-                    data={[
-                        ['x', '한국 확진자수', '전일대비', '사망자', '완치자'],
-                        [0, 0, 0, 15, 15],
-                        [1, 10, 5, 15, 15],
-                        [2, 23, 15, 15, 15],
-                        [3, 17, 9, 15, 15],
-                        [4, 18, 10, 15, 15],
-                        [5, 9, 5, 15, 15],
-                        [6, 11, 3, 15, 15],
-                        [7, 27, 19, 15, 15],
-                    ]}
-
-                    options={{
-                        legend: { position: 'top' },
-                        chartArea: { 'width': '80%', 'height': '80%' },
-                        series: {
-                            1: { curveType: 'function' },
-                        }
+                <Text style={textStyles.title}>CanvasJS Test</Text>
+                <CanvasJSChart 
+                    options = {{
+                        title: {
+                            text: 'Basic chart'
+                        },
+                        data: [{
+                            type: 'column',
+                            dataPoints: [
+                                { label: "Apple",  y: 10  },
+                                { label: "Orange", y: 15  },
+                                { label: "Banana", y: 25  },
+                                { label: "Mango",  y: 30  },
+                                { label: "Grape",  y: 28  }
+                            ]
+                        }]
                     }}
-                    rootProps={{ 'data-testid': '10' }}
-                />
-            </View>
-            <View style={styles.chart}>
-                <Text style={textStyles.title}>한국 확진자수 Geo chart</Text>
-                <Chart
-                    //width={'470px'}
-                    //height={'270px'}
-                    chartType="GeoChart"
-                    data={[
-                        ['City', '확진자', '완치자'],
-                        ['서울', 36, 10],
-                        ['부산', -8, 1],
-                        ['대구', 6, 10],
-                        ['대전', -24, 10],
-                        ['광주', 12, 1],
-                        ['인천', -3, 10],
-                        ['울산', 3, 10],
-                        ['충북', 28, 1],
-                        ['충남', 15, 1],
-                        ['강원', 4, 10],
-                        ['경기', 35, 1],
-                        ['경북', 12, 10],
-                        ['경남', -12, 1],
-                        ["전북", 6, 10],
-                        ['전남', -3, 10],
-                        ['제주', 12, 1],
-                    ]}
-                    options={{
-                        region: 'KR',
-                        colorAxis: { colors: ['#FDF5E6', '#FFE4E1', '#DA70D6'] },
-                        backgroundColor: '#81d4fa',
-                        datalessRegionColor: '#FFFAFA',
-                        defaultColor: '#f5f5f5',
-                        displayMode: 'markers'
-                    }}
-                    // Note: you will need to get a mapsApiKey for your project.
-                    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-                    mapsApiKey="AIzaSyBMdWulC9vKMjat5WfDVp00AYsi-DLpB8Y"
-                    rootProps={{ 'data-testid': '2' }}
-                />
-            </View>
-            <View style={styles.chart}>
-                <Chart
-                    //width={'500px'}
-                    height={'300px'}
-                    chartType="PieChart"
-                    loader={<div>Loading Chart</div>}
-                    data={[
-                        ['City', '확진자'],
-                        ['서울특별시', 36],
-                        ['충청북도', 28],
-                        ['충청남도', 15],
-                        ['강원도', 4],
-                        ['경기도', 35],
-                        ['경상북도', 12],
-                        ['경상남도', 12],
-                        ["전라북도",  10],
-                        ['전라남도',  10],
-                        ['제주특별시', 1],
-                        ['세종특별자치시',  10],
-                    ]}
-                    options={{
-                        title: '한국 확진자수 Pie chart',
-                    }}
-                    rootProps={{ 'data-testid': '1' }}
                 />
             </View>
         </ScrollView>
--- src/Container/KoreaCorona.js
+++ src/Container/KoreaCorona.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, ScrollView, TouchableOpacity, Text } from 'react-native';
-import { serverUrl, totalCorona, regionCorona } from '../url';
+import { serverUrl, totalCorona, regionCorona, dayCorona } from '../url';
 
 import KoreaTotal from '../Component/KoreaTotal';
 import KoreaRegions from '../Component/KoreaRegions';
@@ -13,6 +13,7 @@ const KoreaCorona = () => {
 
     const [totalData, setTotalData] = useState({});
     const [regionsData, setRegionsData] = useState({});
+    const [dayData, setDayData] = useState({});
     
 
     useEffect(()=> {
@@ -35,6 +36,20 @@ const KoreaCorona = () => {
             .then(res => {
                 //console.log(res)
                 setRegionsData(res)
+            })
+            .catch(err => console.log(err))
+    },[])
+
+    useEffect(()=> {
+        fetch(dayCorona,
+                {method: 'get'}
+            )
+            .then(res => res.text())
+            .then(res => {
+                var result = res.toString().trim().slice(93,res.length-1);
+                //console.log(JSON.parse(res.slice(93,res.length-1)))
+                console.log(JSON.parse(result.trim()))
+                //setDayData(res)
             })
             .catch(err => console.log(err))
     },[])
